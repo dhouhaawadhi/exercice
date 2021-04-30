49eb625fe55a8a5568b17e1a0416a8d335c82b01
dhouhaawadhi <78470631+dhouhaawadhi@users.noreply.github.com>
2021-04-30 16:43:15 +0200

    Add files via upload

---
+++ ex.js
@@ -0,0 +1,17 @@
+
+
+
+
+
+
+
+let colorInput=document.querySelector('color');
+
+colorInput.addEventListener('input',()=>{
+  let color =colorInput.Value;
+  document.querySelector('h4').style.color=color;
+
+
+});
+
+
